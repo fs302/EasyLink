8b653cf82c909013024eee4e2bc1b7c817465ce7
fanshen.fs <fanshen.fs@alibaba-inc.com>
2022-02-06 22:53:06 +0800

    refactor: seal pipeline on local dataset

---
+++ easylink/common/data_utils.py
@@ -0,0 +1,75 @@
+import sys
+import numpy as np
+import networkx as nx
+import scipy.sparse as ss
+
+def load_basic_network(file_path, graph_type='undirected'):
+    g = nx.DiGraph() if graph_type == 'directed' else nx.Graph()
+    try:
+        with open(file_path) as f:
+            print("loading file:", file_path)
+            for l in f:
+                data = l.strip().split()
+                id1 = int(data[0])
+                id2 = int(data[1])
+                if len(data) > 2:
+                    weight = float(data[2])
+                else:
+                    weight = 1.0
+                g.add_edge(id1, id2, weight=weight)
+    except IOError:
+        print("error:", sys.exc_info()[0])
+        raise
+    print("#nodes:",g.number_of_nodes(),",#edges:",g.number_of_edges())
+    return g
+
+def sparse_to_tuple(sparse_mx):
+    if not ss.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+def train_test_split(nx_adj, test_frac=.2, val_frac=.1, pos_neg_ratio=.5):
+
+    # Remove diagonal elements
+    nx_adj = nx_adj - ss.dia_matrix((nx_adj.diagonal()[np.newaxis, :], [0]), shape=nx_adj.shape)
+    nx_adj.eliminate_zeros()
+
+    g = nx.from_scipy_sparse_matrix(nx_adj)
+
+    adj_triu = ss.triu(nx_adj)
+    edges, weights, adj_shape = sparse_to_tuple(adj_triu)
+
+    edge_tuples = [(min(edge[0], edge[1]), max(edge[0], edge[1])) for edge in edges]
+    np.random.shuffle(edge_tuples)
+
+    num_test = int(np.floor(edges.shape[0] * test_frac))
+    num_val = int(np.floor(edges.shape[0] * val_frac))
+    test_edges = edge_tuples[:num_test]
+    val_edges = edge_tuples[num_test:num_test+num_val]
+    train_edges = edge_tuples[num_test+num_val:]
+
+    print("Negative Sampling.")
+    all_edge_set = set(edge_tuples)
+    neg_edge_set = set()
+    nodes_set = g.nodes
+    while len(neg_edge_set) < len(edge_tuples)/pos_neg_ratio:
+        idx_i = np.random.randint(0, nx_adj.shape[0])
+        idx_j = np.random.randint(0, nx_adj.shape[0])
+        if idx_i == idx_j or idx_i not in nodes_set or idx_j not in nodes_set:
+            continue
+        neg_edge = (idx_i, idx_j)
+        if neg_edge in all_edge_set or neg_edge in neg_edge_set:
+            continue
+        neg_edge_set.add(neg_edge)
+
+    neg_edge_tuples = list(neg_edge_set)
+    num_test_neg = int(num_test/pos_neg_ratio)
+    num_val_neg = int(num_val/pos_neg_ratio)
+    test_neg_edges = neg_edge_tuples[:num_test_neg]
+    val_neg_edges = neg_edge_tuples[num_test_neg:num_test_neg + num_val_neg]
+    train_neg_edges = neg_edge_tuples[num_test_neg + num_val_neg:]
+
+    return train_edges, test_edges, val_edges, train_neg_edges, test_neg_edges, val_neg_edges
---
+++ example/seal_pipe.py
@@ -0,0 +1,169 @@
+import argparse
+from codecs import ignore_errors
+from random import shuffle
+import time
+import os, sys, shutil
+from tqdm import tqdm
+import numpy as np
+import scipy.sparse as ssp
+import networkx as nx
+from sklearn.metrics import roc_auc_score
+import torch
+from torch_geometric.data import Data, Dataset, InMemoryDataset
+from torch_geometric.loader import DataLoader
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
+
+parent_path = os.path.dirname(sys.path[0])
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+from easylink.common.eval_utils import evaluate_auc
+from easylink.model.heuristic_similarity import common_neighbors, adamic_adar, resource_allocation, local_path_index
+from easylink.model.seal import SEAL, SEALDataset
+from easylink.common.data_utils import load_basic_network, train_test_split
+
+@torch.no_grad()
+def test(dataset, seal, batch_size, evaluator=None):
+    seal.model.eval()
+    data_loader = DataLoader(dataset, batch_size)
+    pbar = tqdm(data_loader, ncols=80)
+    y_pred, y_true = [], []
+    for data in pbar:
+        data = data.to(device)
+        x = data.x if args.use_feature else None
+        logits = seal.model(data.z, data.edge_index, data.batch, x)
+        y_pred.append(logits.view(-1).cpu())
+        y_true.append(data.y.view(-1).cpu().to(torch.float))
+    val_pred, val_true = torch.cat(y_pred), torch.cat(y_true)
+    pos_val_pred = val_pred[val_true==1]
+    neg_val_pred = val_pred[val_true==0]
+
+    result = {}
+    auc = roc_auc_score(val_true, val_pred)
+    print("AUC:{}".format(auc))
+    result['AUC'] = auc
+
+    if evaluator is not None:
+        for K in [20, 50, 100]:
+            evaluator.K = K
+            valid_hits = evaluator.eval({
+                    'y_pred_pos': pos_val_pred,
+                    'y_pred_neg': neg_val_pred,
+                })[f'hits@{K}']
+            result[f'Hits@{K}'] = valid_hits
+            print(f"Hits@{K}:{valid_hits}")
+
+    return result
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SEAL LinkPredictor')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--log_steps', type=int, default=1)
+    parser.add_argument('--dataset', type=str, default='ogbl-collab')
+    parser.add_argument('--use_cache', action='store_true')
+    # DataStructure settings
+    parser.add_argument('--use_feature', action='store_true', 
+                    help="whether to use raw node features as GNN input")
+    parser.add_argument('--num_hops', type=int, default=2)
+    parser.add_argument('--max_nodes_per_hop', type=int, default=10)
+    # GNN settings
+    parser.add_argument('--model', type=str, default='SAGE')
+    parser.add_argument('--sortpool_k', type=float, default=0.6)
+    parser.add_argument('--num_layers', type=int, default=3)
+    parser.add_argument('--hidden_channels', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--dropout', type=float, default=0.5)
+
+    # Training settings
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--epochs', type=int, default=5)
+    args = parser.parse_args()
+    print(args)
+    
+    # Data Preparation
+    if args.dataset.startswith('ogbl'):
+        print("Loading data.")
+        dataset_name = args.dataset
+        dataset_root = '../data'
+        dataset = PygLinkPropPredDataset(
+            name=dataset_name, root=dataset_root)
+        evaluator = Evaluator(dataset_name)
+        split_edge = dataset.get_edge_split()
+        graph = dataset[0]
+        train_pos_edges = split_edge['train']['edge'][:200000]
+        val_pos_edge, val_neg_edge = split_edge['valid']['edge'], split_edge['valid']['edge_neg']
+        test_pos_edge, test_neg_edge = split_edge['test']['edge'], split_edge['test']['edge_neg']
+        
+        # Training
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        max_z = 10000
+        node_feat = graph.x if args.use_feature else None
+        base_edges = graph.edge_index.t()
+        seal = SEAL('SAGE', False, args.lr, args.hidden_channels, args.num_layers, max_z, args.dropout)
+        train_dataset_dir = dataset.root+'_seal'
+        if not args.use_cache:
+            shutil.rmtree(train_dataset_dir, ignore_errors=True)
+        train_dataset = SEALDataset(train_dataset_dir, base_edges, train_pos_edges, 
+                        graph.num_nodes, args.num_hops, args.max_nodes_per_hop, node_feat=node_feat)
+        seal.train(train_dataset, args.epochs, args.batch_size, device)
+
+        # Test
+        val_dataset_dir = dataset.root+"_seal_val"
+        if not args.use_cache:
+            shutil.rmtree(val_dataset_dir, ignore_errors=True)
+        val_dataset = SEALDataset(val_dataset_dir, base_edges, val_pos_edge,
+                                graph.num_nodes, args.num_hops, args.max_nodes_per_hop,
+                                node_feat=node_feat, neg_edges=val_neg_edge)
+        test(val_dataset, seal, args.batch_size, evaluator)
+    else:
+        dataset_root = '../data/facebook'
+        # facebook
+        facebook_dir = dataset_root+'/facebook.txt'
+        g = load_basic_network(facebook_dir)
+        adj = nx.adjacency_matrix(g)
+        train_edges, test_edges, val_edges, train_neg_edges, test_neg_edges, val_neg_edges = train_test_split(adj)
+
+        test_heuristics = False
+        if test_heuristics:
+            train_edges = np.array(train_edges)
+            train_edges_reverse = np.array([train_edges[:,1], train_edges[:,0]]).transpose()
+            train_edges = np.concatenate([train_edges, train_edges_reverse], axis=0)
+            edge_weight = torch.ones(train_edges.shape[0], dtype=int)
+            A = ssp.csr_matrix(
+                (edge_weight, (train_edges[:,0], train_edges[:,1])), shape = (g.number_of_nodes(), g.number_of_nodes())
+            )
+            h_predictor = common_neighbors 
+            val_edges = torch.tensor(val_edges)
+            val_neg_edges = torch.tensor(val_neg_edges)
+            pos_valid_pred = h_predictor(A, val_edges, batch_size=args.batch_size)
+            neg_valid_pred = h_predictor(A, val_neg_edges, batch_size=args.batch_size)
+            val_pred = torch.cat([torch.Tensor(pos_valid_pred), torch.Tensor(neg_valid_pred)])
+            val_true = torch.cat([torch.ones(pos_valid_pred.shape[0], dtype=int),
+                        torch.zeros(neg_valid_pred.shape[0], dtype=int)]) 
+            auc = roc_auc_score(val_true, val_pred)
+            print("Heuristic AUC:{}".format(auc)) 
+        else:
+            train_edges = np.array(train_edges)
+            train_edges_reverse = np.array([train_edges[:,1], train_edges[:,0]]).transpose()
+            train_edges = np.concatenate([train_edges, train_edges_reverse], axis=0)
+            train_edges = torch.tensor(train_edges)
+            val_edges = torch.tensor(val_edges)
+            val_neg_edges = torch.tensor(val_neg_edges)
+            # Training
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            max_z = 1000
+            seal = SEAL('SAGE', False, args.lr, args.hidden_channels, args.num_layers, max_z, args.dropout)
+            train_dataset_dir = dataset_root+'_seal'
+            if not args.use_cache:
+                shutil.rmtree(train_dataset_dir, ignore_errors=True)
+            train_dataset = SEALDataset(train_dataset_dir, train_edges, train_edges[:100000], 
+                            g.number_of_nodes(), args.num_hops, args.max_nodes_per_hop, node_feat=None)
+            seal.train(train_dataset, args.epochs, args.batch_size, device)
+
+            # Test
+            val_dataset_dir = dataset_root+"_seal_val"
+            if not args.use_cache:
+                shutil.rmtree(val_dataset_dir, ignore_errors=True)
+            val_dataset = SEALDataset(val_dataset_dir, train_edges, val_edges,
+                                    g.number_of_nodes(), args.num_hops, args.max_nodes_per_hop,
+                                    node_feat=None, neg_edges=val_neg_edges)
+            test(val_dataset, seal, args.batch_size, None)
